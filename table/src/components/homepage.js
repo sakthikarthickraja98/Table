160286718f1d8194e94cf95ad285d09909b476a4
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-10-24 21:04:29 +0530

    search bar

--- table/src/components/homepage.js
+++ table/src/components/homepage.js
@@ -1,14 +1,74 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useEffect, useState} from 'react';
 import Table from './table';
 import SearchBar from './searchbar';
+import './searchbar.css';
+
+
+import ReactPaginate from 'react-paginate';
 
 const Homepage = () => {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [term, setTerm] = useState("");
+
+    const handlePageClick = (data) => {
+        let currentPage = data.selected + 1
+        setCurrentPage(currentPage);
+    }
+
+    const fetchBlogs = async() => {
+            setLoading(true);
+            const res = await axios.get(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=10`);
+            setData(res.data);
+    };
+
+    useEffect(() => {fetchBlogs()},[currentPage]);
+
+    function search(rows) {
+        return rows.filter((row) => row.name.toLowerCase().indexOf(term) > -1);
+    }
+
     return (
     <>
-    <SearchBar/>
+    {/* <SearchBar/> */}
+    <div className="topnav">
+<div class="search-container" style={{padding:"10px"}}>
+    <form>
+      <input type="text" placeholder="Search.." name="search" onChange={(e)=>setTerm(e.target.value)} value={term}/>
+      {/* <button><i class="fa fa-search"></i></button> */}
+    </form>
+  </div>
+</div>
+
     <div className="container" style={{marginTop:"20px"}}>
-        <Table/>
+        <Table data={search(data)}/>
     </div>
+
+    <div style={{display:'flex', alignItems:'center',justifyContent:'center',marginTop:'22px'}}>
+            <ReactPaginate
+             previousLabel={'previous'}
+             nextLable={'next'}
+             breakLable={'...'}
+             pageCount={33}
+             marginPagesDisplayed={3}
+             pageRangeDisplayed={2}
+             onPageChange={handlePageClick}
+             containerClassName={'pagination justify-content-center'}
+             pageClassName={'page-item'}
+             pageLinkClassName={'page-link'}
+             previousClassName={'page-item'}
+             previousLinkClassName={'page-link'}
+             nextClassName={'page-item'}
+             nextLinkClassName={'page-link'}
+             breakClassName={'page-item'}
+             breakLinkClassName={'page-link'}
+             activeClassName={'active'}
+            />
+    </div>
+
     </>
     );
 }
--- table/src/components/table.js
+++ table/src/components/table.js
@@ -5,32 +5,13 @@ import './table.css';
 import ReactPaginate from 'react-paginate';
 
 
-const Table = () => {
-
-    const [tableDetails, setTableDetails] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-
-
-    const handlePageClick = (data) => {
-
-        let currentPage = data.selected + 1
-        setCurrentPage(currentPage);
-    }
-
-    const fetchBlogs = async() => {
-            setLoading(true);
-            const res = await axios.get(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=10`);
-            setTableDetails(res.data);
-    };
-
-    useEffect(() => {fetchBlogs()},[currentPage]);
+const Table = ({data}) => {
     
-    if (!loading){
-        return <h4>Loading...</h4>;
-    }
-
-    const columns = {tableDetails}[0] && Object.keys({tableDetails}[0]);
+    // if (!loading){
+    //     return <h4>Loading...</h4>;
+    // }
+   
+    // const columns = data[0] && Object.keys(data[0]);
     return (
         <div>
         <div className="tableContainer" >
@@ -43,10 +24,11 @@ const Table = () => {
                         <th>Ph</th>
                         <th>Description</th>
                         <th>Brewers Tips</th>
+                        {/* {data[0] && columns.map((heading)=><th>{heading}</th>)} */}
                     </tr>
                 </thead>
                 <tbody>
-                        {tableDetails.map(item => 
+                        {data.map(item => 
                         <tr key={item.name}>
                         <td data-label="Name">{item.name}</td>
                         <td data-label="Tagline">{item.tagline}</td>
@@ -56,30 +38,15 @@ const Table = () => {
                         <td data-label="Brewers Tips">{item.brewers_tips}</td>
                         </tr>
                         )}
+                        {/* {data.map(row => <tr>
+                            {
+                                columns.map(column => <td>{row[column]}</td>)
+                            }
+                        </tr>)} */}
                 </tbody>
             </table>
 
-        <div style={{display:'flex', alignItems:'center',justifyContent:'center',marginTop:'22px'}}>
-            <ReactPaginate
-             previousLabel={'previous'}
-             nextLable={'next'}
-             breakLable={'...'}
-             pageCount={33}
-             marginPagesDisplayed={3}
-             pageRangeDisplayed={2}
-             onPageChange={handlePageClick}
-             containerClassName={'pagination justify-content-center'}
-             pageClassName={'page-item'}
-             pageLinkClassName={'page-link'}
-             previousClassName={'page-item'}
-             previousLinkClassName={'page-link'}
-             nextClassName={'page-item'}
-             nextLinkClassName={'page-link'}
-             breakClassName={'page-item'}
-             breakLinkClassName={'page-link'}
-             activeClassName={'active'}
-            />
-        </div>
+
             
         </div>
         </div>
